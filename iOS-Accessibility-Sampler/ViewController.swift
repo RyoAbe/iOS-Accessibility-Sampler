1879aca0aa8cc22d6104ec35d9d79763fa8e4897
Ryo Abe <ryo.abe@c-fo.com>
2020-10-18 20:54:56 +0900

    initial commit

---
+++ iOS-Accessibility-Sampler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Accessibility-Sampler
+//
+//  Created by Ryo Abe on 2020/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
