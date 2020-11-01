81305e1945ce3d0212663961ade395ebe900fed8
Ryo Abe <ryo.abe@c-fo.com>
2020-11-01 22:10:05 +0900

    リファクタリング

---
+++ iOS-Accessibility-Sampler/Documentation.swift
@@ -0,0 +1,106 @@
+import UIKit
+
+struct Page {
+    let title: String
+    let type: PageType
+}
+
+enum PageType {
+    case topic(Topic)
+    case article(url: String)
+    case `protocol`
+    case `struct`
+    case `var`
+}
+
+extension PageType {
+    var image: UIImage? {
+        switch self {
+        case .topic:
+            return UIImage(systemName: "doc.plaintext")
+        case .article:
+            return UIImage(systemName: "doc")
+        case .protocol:
+            return UIImage(systemName: "chevron.left.slash.chevron.right")
+        case .struct:
+            return UIImage(systemName: "chevron.left.slash.chevron.right")
+        case .var:
+            return UIImage(systemName: "chevron.left.slash.chevron.right")
+        }
+    }
+}
+
+struct Topic {
+    let title: String
+    let sections: [Section]
+}
+
+struct Section {
+    let title: String
+    let pages: [Page]
+}
+
+struct Documentation {
+    static let shared = Documentation()
+    private init() {}
+    static let top = Topic(
+        title: "Accessibility for iOS and tvOS",
+        sections: [
+            .init(
+                title: "Essentials",
+                pages: [
+                    .init(title: "UIAccessibility", type: .topic(topicUIAccessibility)),
+                    .init(title: "UIAccessibilityContainer", type: .protocol),
+                    .init(title: "Supporting VoiceOver in Your App", type: .protocol),
+                ]
+            ),
+            .init(
+                title: "Behaviors",
+                pages: [
+                    .init(title: "UIAccessibilityFocus", type: .protocol),
+                    .init(title: "UIAccessibilityIdentification", type: .protocol),
+                    .init(title: "UIAccessibilityReadingContent", type: .protocol),
+                    .init(title: "UIAccessibilityContentSizeCategoryImageAdjusting", type: .protocol),
+                    .init(title: "UIAccessibilityTextualContext", type: .protocol),
+                ]
+            ),
+            .init(title: "Behaviors", pages: []),
+            .init(title: "Guided Access", pages: []),
+            .init(title: "Actions", pages: []),
+            .init(title: "Elements", pages: []),
+            .init(title: "Containers", pages: []),
+            .init(title: "Navigation", pages: []),
+            .init(title: "Drag and Drop Support", pages: []),
+            .init(title: "Notifications", pages: []),
+            .init(title: "Conversions", pages: []),
+            .init(title: "Convenience Functions", pages: []),
+            .init(title: "Capabilities", pages: [])
+        ]
+    )
+
+    static let topicUIAccessibility = Topic(
+        title: "UIAccessibility",
+        sections: [
+            .init(
+                title: "Determining Accessibility",
+                pages: [
+                    .init(title: "isAccessibilityElement", type: .var)
+                ]
+            ),
+            .init(
+                title: "Configuring Behavior",
+                pages: [
+                    .init(title: "Accessibility Traits", type: .protocol),
+                    .init(title: "var accessibilityTraits: UIAccessibilityTraits", type: .var),
+                    .init(title: "struct UIAccessibilityTraits", type: .var),
+                    .init(title: "var accessibilityCustomRotors: [UIAccessibilityCustomRotor]?", type: .var),
+                    .init(title: "var accessibilityElementsHidden: Bool", type: .var),
+                    .init(title: "var accessibilityNotifiesWhenDestroyed: Bool", type: .var),
+                    .init(title: "var accessibilityRespondsToUserInteraction: Bool", type: .var),
+                    .init(title: "var accessibilityViewIsModal: Bool", type: .var),
+                    .init(title: "var shouldGroupAccessibilityChildren: Bool", type: .var)
+                ]
+            ),
+        ]
+    )
+}
---
+++ iOS-Accessibility-Sampler/TopicViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  iOS-Accessibility-Sampler
+//
+//  Created by Ryo Abe on 2020/10/18.
+//
+
+import UIKit
+import SafariServices
+
+class TopicViewController: UITableViewController {
+    private var topic: Topic!
+
+    static func make(with topic: Topic) -> TopicViewController {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(identifier: "Topic") as! TopicViewController
+        vc.topic = topic
+        return vc
+    }
+
+    required init?(coder: NSCoder) {
+        self.topic = Documentation.top
+        super.init(coder: coder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = topic.title
+    }
+}
+
+extension TopicViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        topic.sections.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        topic.sections[section].title
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        topic.sections[section].pages.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let page = topic.sections[indexPath.section].pages[indexPath.row]
+        cell.textLabel?.text = page.title
+        cell.imageView?.image = page.type.image
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let page = topic.sections[indexPath.section].pages[indexPath.row]
+        switch page.type {
+        case .topic(let topic):
+            let vc = TopicViewController.make(with: topic)
+            navigationController?.pushViewController(vc, animated: true)
+        case .article(let url):
+            let vc = SFSafariViewController(url: URL(string: url)!)
+            present(vc, animated: true)
+        case .protocol:
+            break
+        case .struct:
+            break
+        case .var:
+            break
+        }
+    }
+}
