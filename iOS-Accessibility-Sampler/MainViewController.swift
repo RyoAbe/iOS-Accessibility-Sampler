260f3f5bbaa2de9021f0ab6963f5327701bd8f29
Ryo Abe <ryo.abe@c-fo.com>
2020-11-01 18:59:52 +0900

    add topics

--- iOS-Accessibility-Sampler/MainViewController.swift
+++ iOS-Accessibility-Sampler/MainViewController.swift
@@ -8,35 +8,65 @@
 import UIKit
 
 class MainViewController: UITableViewController {
-    private let topics = [
-        "Essentials",
-        "Behaviors",
-        "Guided Access",
-        "Actions",
-        "Elements",
-        "Containers",
-        "Navigation",
-        "Drag and Drop Support",
-        "Notifications",
-        "Conversions",
-        "Convenience Functions",
-        "Capabilities"
+    enum ItemType {
+        case topic, article, `protocol`, `struct`
+        var image: UIImage? {
+            switch self {
+            case .topic:
+                return UIImage(systemName: "doc.plaintext")
+            case .article:
+                return UIImage(systemName: "doc")
+            case .protocol:
+                return UIImage(systemName: "chevron.left.slash.chevron.right")
+            case .struct:
+                return UIImage(systemName: "chevron.left.slash.chevron.right")
+            }
+        }
+    }
+    private let docs: [(title: String, rows: [(type: ItemType, title: String)])] = [
+        ("Essentials", [
+            (.topic, "UIAccessibility"),
+            (.topic, "UIAccessibilityContainer"),
+            (.article, "Supporting VoiceOver in Your App")
+        ]),
+        ("Behaviors", [
+            (.topic, "UIAccessibilityFocus"),
+            (.protocol, "UIAccessibilityIdentification"),
+            (.protocol, "UIAccessibilityReadingContent"),
+            (.protocol, "UIAccessibilityContentSizeCategoryImageAdjusting"),
+            (.struct, "UIAccessibilityTextualContext")
+        ]),
+        ("Guided Access", []),
+        ("Actions", []),
+        ("Elements", []),
+        ("Containers", []),
+        ("Navigation", []),
+        ("Drag and Drop Support", []),
+        ("Notifications", []),
+        ("Conversions", []),
+        ("Convenience Functions", []),
+        ("Capabilities", [])
     ]
 }
 
 extension MainViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        docs.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        topics.count
+        docs[section].rows.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        docs[section].title
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let topic = topics[indexPath.row]
-        cell.textLabel?.text = topic
+        let row = docs[indexPath.section].rows[indexPath.row]
+        cell.textLabel?.text = row.title
+        cell.imageView?.image = row.type.image
         return cell
     }
 }
