47b5297963d28019a9732088dc67108dc00acded
Ryo Abe <ryo.abe@c-fo.com>
2020-11-02 10:48:41 +0900

    リファクタリング

--- iOS-Accessibility-Sampler/Documentation.swift
+++ iOS-Accessibility-Sampler/Documentation.swift
@@ -1,4 +1,68 @@
 import UIKit
+struct Documentation {
+    static let shared = Documentation()
+    private init() {}
+    static let top = Topic(
+        title: "Accessibility for iOS and tvOS",
+        sections: [
+            .init(
+                title: "Essentials",
+                pages: [
+                    .init(title: "UIAccessibility", type: .topic(uiAccessibility)),
+                    .init(title: "UIAccessibilityContainer", type: .protocol),
+                    .init(title: "Supporting VoiceOver in Your App", type: .article(url: "https://developer.apple.com/documentation/uikit/accessibility_for_ios_and_tvos/supporting_voiceover_in_your_app")),
+                ]
+            ),
+            .init(
+                title: "Behaviors",
+                pages: [
+                    .init(title: "UIAccessibilityFocus", type: .protocol),
+                    .init(title: "UIAccessibilityIdentification", type: .protocol),
+                    .init(title: "UIAccessibilityReadingContent", type: .protocol),
+                    .init(title: "UIAccessibilityContentSizeCategoryImageAdjusting", type: .protocol),
+                    .init(title: "UIAccessibilityTextualContext", type: .protocol),
+                ]
+            ),
+            .init(title: "Behaviors", pages: []),
+            .init(title: "Guided Access", pages: []),
+            .init(title: "Actions", pages: []),
+            .init(title: "Elements", pages: []),
+            .init(title: "Containers", pages: []),
+            .init(title: "Navigation", pages: []),
+            .init(title: "Drag and Drop Support", pages: []),
+            .init(title: "Notifications", pages: []),
+            .init(title: "Conversions", pages: []),
+            .init(title: "Convenience Functions", pages: []),
+            .init(title: "Capabilities", pages: [])
+        ]
+    )
+
+    static let uiAccessibility = Topic(
+        title: "UIAccessibility",
+        sections: [
+            .init(
+                title: "Determining Accessibility",
+                pages: [
+                    .init(title: "isAccessibilityElement", type: .var)
+                ]
+            ),
+            .init(
+                title: "Configuring Behavior",
+                pages: [
+                    .init(title: "Accessibility Traits", type: .protocol),
+                    .init(title: "var accessibilityTraits: UIAccessibilityTraits", type: .var),
+                    .init(title: "struct UIAccessibilityTraits", type: .var),
+                    .init(title: "var accessibilityCustomRotors: [UIAccessibilityCustomRotor]?", type: .var),
+                    .init(title: "var accessibilityElementsHidden: Bool", type: .var),
+                    .init(title: "var accessibilityNotifiesWhenDestroyed: Bool", type: .var),
+                    .init(title: "var accessibilityRespondsToUserInteraction: Bool", type: .var),
+                    .init(title: "var accessibilityViewIsModal: Bool", type: .var),
+                    .init(title: "var shouldGroupAccessibilityChildren: Bool", type: .var)
+                ]
+            ),
+        ]
+    )
+}
 
 struct Page {
     let title: String
@@ -40,67 +104,3 @@ struct Section {
     let pages: [Page]
 }
 
-struct Documentation {
-    static let shared = Documentation()
-    private init() {}
-    static let top = Topic(
-        title: "Accessibility for iOS and tvOS",
-        sections: [
-            .init(
-                title: "Essentials",
-                pages: [
-                    .init(title: "UIAccessibility", type: .topic(topicUIAccessibility)),
-                    .init(title: "UIAccessibilityContainer", type: .protocol),
-                    .init(title: "Supporting VoiceOver in Your App", type: .protocol),
-                ]
-            ),
-            .init(
-                title: "Behaviors",
-                pages: [
-                    .init(title: "UIAccessibilityFocus", type: .protocol),
-                    .init(title: "UIAccessibilityIdentification", type: .protocol),
-                    .init(title: "UIAccessibilityReadingContent", type: .protocol),
-                    .init(title: "UIAccessibilityContentSizeCategoryImageAdjusting", type: .protocol),
-                    .init(title: "UIAccessibilityTextualContext", type: .protocol),
-                ]
-            ),
-            .init(title: "Behaviors", pages: []),
-            .init(title: "Guided Access", pages: []),
-            .init(title: "Actions", pages: []),
-            .init(title: "Elements", pages: []),
-            .init(title: "Containers", pages: []),
-            .init(title: "Navigation", pages: []),
-            .init(title: "Drag and Drop Support", pages: []),
-            .init(title: "Notifications", pages: []),
-            .init(title: "Conversions", pages: []),
-            .init(title: "Convenience Functions", pages: []),
-            .init(title: "Capabilities", pages: [])
-        ]
-    )
-
-    static let topicUIAccessibility = Topic(
-        title: "UIAccessibility",
-        sections: [
-            .init(
-                title: "Determining Accessibility",
-                pages: [
-                    .init(title: "isAccessibilityElement", type: .var)
-                ]
-            ),
-            .init(
-                title: "Configuring Behavior",
-                pages: [
-                    .init(title: "Accessibility Traits", type: .protocol),
-                    .init(title: "var accessibilityTraits: UIAccessibilityTraits", type: .var),
-                    .init(title: "struct UIAccessibilityTraits", type: .var),
-                    .init(title: "var accessibilityCustomRotors: [UIAccessibilityCustomRotor]?", type: .var),
-                    .init(title: "var accessibilityElementsHidden: Bool", type: .var),
-                    .init(title: "var accessibilityNotifiesWhenDestroyed: Bool", type: .var),
-                    .init(title: "var accessibilityRespondsToUserInteraction: Bool", type: .var),
-                    .init(title: "var accessibilityViewIsModal: Bool", type: .var),
-                    .init(title: "var shouldGroupAccessibilityChildren: Bool", type: .var)
-                ]
-            ),
-        ]
-    )
-}
