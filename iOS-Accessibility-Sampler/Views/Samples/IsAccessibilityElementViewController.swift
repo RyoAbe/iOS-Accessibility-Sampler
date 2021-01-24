b245f5f18c6c971948264929c6ab3b0e7e66d1ce
Ryo Abe <ryo.abe@c-fo.com>
2021-01-24 14:30:50 +0900

    isAccessibilityElement

--- iOS-Accessibility-Sampler/Views/Samples/IsAccessibilityElementViewController.swift
+++ iOS-Accessibility-Sampler/Views/Samples/IsAccessibilityElementViewController.swift
@@ -7,17 +7,11 @@
 
 import UIKit
 
-class IsAccessibilityElementViewController: UITableViewController {
-    @IBOutlet private weak var cell3: UITableViewCell!
+class IsAccessibilityElementViewController: UIViewController {
     static func make(with api: API) -> IsAccessibilityElementViewController {
         let sb = UIStoryboard(name: "IsAccessibilityElement", bundle: nil)
         let vc = sb.instantiateInitialViewController() as! IsAccessibilityElementViewController
         vc.title = api.rawValue
         return vc
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        cell3.isAccessibilityElement = false
     }
 }
