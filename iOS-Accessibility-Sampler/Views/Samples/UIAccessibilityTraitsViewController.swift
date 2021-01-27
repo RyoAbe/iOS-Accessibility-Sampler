c6d1db9f631101ba5387950807f92e4c54aca9a4
Ryo Abe <ryo.abe@c-fo.com>
2021-01-27 23:29:35 +0900

    補足分追加

--- iOS-Accessibility-Sampler/Views/Samples/UIAccessibilityTraitsViewController.swift
+++ iOS-Accessibility-Sampler/Views/Samples/UIAccessibilityTraitsViewController.swift
@@ -29,10 +29,6 @@ class UIAccessibilityTraitsViewController: UITableViewController {
         .tabBar
     ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -46,47 +42,94 @@ class UIAccessibilityTraitsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let trait = traits[indexPath.row]
-        switch trait {
-        case .none:
-            cell.textLabel?.text = "accessibilityTraits = [.none]"
-        case .button:
-            cell.textLabel?.text = "accessibilityTraits = [.button]"
-        case .link:
-            cell.textLabel?.text = "accessibilityTraits = [.link]"
-        case .header:
-            cell.textLabel?.text = "accessibilityTraits = [.header]"
-        case .searchField:
-            cell.textLabel?.text = "accessibilityTraits = [.searchField]"
-        case .image:
-            cell.textLabel?.text = "accessibilityTraits = [.image]"
-        case .selected:
-            cell.textLabel?.text = "accessibilityTraits = [.selected]"
-        case .playsSound:
-            cell.textLabel?.text = "accessibilityTraits = [.playsSound]"
-        case .keyboardKey:
-            cell.textLabel?.text = "accessibilityTraits = [.keyboardKey]"
-        case .staticText:
-            cell.textLabel?.text = "accessibilityTraits = [.staticText]"
-        case .summaryElement:
-            cell.textLabel?.text = "accessibilityTraits = [.summaryElement]"
-        case .notEnabled:
-            cell.textLabel?.text = "accessibilityTraits = [.notEnabled]"
-        case .updatesFrequently:
-            cell.textLabel?.text = "accessibilityTraits = [.updatesFrequently]"
-        case .startsMediaSession:
-            cell.textLabel?.text = "accessibilityTraits = [.startsMediaSession]"
-        case .adjustable:
-            cell.textLabel?.text = "accessibilityTraits = [.adjustable]"
-        case .allowsDirectInteraction:
-            cell.textLabel?.text = "accessibilityTraits = [.allowsDirectInteraction]"
-        case .causesPageTurn:
-            cell.textLabel?.text = "accessibilityTraits = [.causesPageTurn]"
-        case .tabBar:
-            cell.textLabel?.text = "accessibilityTraits = [.tabBar]"
-        default:
-            break
-        }
+        cell.textLabel?.text = "accessibilityTraits = [.\(trait.title ?? "")]"
+        cell.detailTextLabel?.text = trait.subtitle
         cell.accessibilityTraits = [trait]
         return cell
+    }
+}
+
+private extension UIAccessibilityTraits {
+    var title: String? {
+        switch self {
+        case .none:
+            return "none"
+        case .button:
+            return "button"
+        case .link:
+            return "link"
+        case .header:
+            return "header"
+        case .searchField:
+            return "searchField"
+        case .image:
+            return "image"
+        case .selected:
+            return "selected"
+        case .playsSound:
+            return "playsSound"
+        case .keyboardKey:
+            return "keyboardKey"
+        case .staticText:
+            return "staticText"
+        case .summaryElement:
+            return "summaryElement"
+        case .notEnabled:
+            return "notEnabled"
+        case .updatesFrequently:
+            return "updatesFrequently"
+        case .startsMediaSession:
+            return "startsMediaSession"
+        case .adjustable:
+            return "adjustable"
+        case .allowsDirectInteraction:
+            return "allowsDirectInteraction"
+        case .causesPageTurn:
+            return "causesPageTurn"
+        case .tabBar:
+            return "tabBar"
+        default:
+            return nil
+        }
+    }
+    var subtitle: String? {
+        switch self {
+        case .none:
+            return "The accessibility element has no traits."
+        case .button:
+            return "The accessibility element should be treated as a button."
+        case .link:
+            return "The accessibility element should be treated as a link."
+        case .searchField:
+            return "The accessibility element should be treated as a search field."
+        case .image:
+            return "The accessibility element should be treated as an image."
+        case .selected:
+            return "The accessibility element is currently selected."
+        case .playsSound:
+            return "The accessibility element plays its own sound when activated."
+        case .keyboardKey:
+            return "The accessibility element behaves as a keyboard key."
+        case .staticText:
+            return "The accessibility element should be treated as static text that cannot change."
+        case .summaryElement:
+            return "The accessibility element provides summary information when the application starts."
+        case .notEnabled:
+            return "The accessibility element is not enabled and does not respond to user interaction."
+        case .updatesFrequently:
+            return "The accessibility element frequently updates its label or value."
+        case .startsMediaSession:
+            return "The accessibility element starts a media session when it is activated."
+        case .adjustable:
+            return "The accessibility element allows continuous adjustment through a range of values."
+        case .allowsDirectInteraction:
+            return "The accessibility element allows direct touch interaction for VoiceOver users."
+        case .causesPageTurn:
+            return "The accessibility element should cause an automatic page turn when VoiceOver finishes reading the text within it."
+        case .header:
+            return "The accessibility element is a header that divides content into sections, such as the title of a navigation bar."
+        default:
+            return nil
+        }
     }
 }
