8e40863a5b8505195fcc9aff775aad8feeb8794a
Ryo Abe <ryo.abe@c-fo.com>
2021-02-21 21:41:04 +0900

    クラスを追加

---
+++ iOS-Accessibility-Sampler/Views/Samples/AccessibilityRespondsToUserInteractionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AccessibilityRespondsToUserInteractionViewController.swift
+//  iOS-Accessibility-Sampler
+//
+//  Created by Ryo Abe on 2021/02/21.
+//
+
+import UIKit
+
+class AccessibilityRespondsToUserInteractionViewController: UIViewController {
+
+}
