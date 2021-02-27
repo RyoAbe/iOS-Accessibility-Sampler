58a3fa9533dfe3316fbf549383471a0395757094
Ryo Abe <ryo.abe@c-fo.com>
2021-02-27 17:26:14 +0900

    ViewController追加

---
+++ iOS-Accessibility-Sampler/Views/Samples/AccessibilityViewIsModalViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AccessibilityViewIsModalViewController.swift
+//  iOS-Accessibility-Sampler
+//
+//  Created by Ryo Abe on 2021/02/27.
+//
+
+import UIKit
+
+class AccessibilityViewIsModalViewController: UIViewController {
+
+}
