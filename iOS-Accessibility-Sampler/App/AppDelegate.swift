d9244eb030ac70eb50b207abbcf75db3dd8dbf0b
Ryo Abe <ryo.abe@c-fo.com>
2021-10-11 13:58:00 +0900

    fix iOS 15 Navigation Bar Transparent

--- iOS-Accessibility-Sampler/App/AppDelegate.swift
+++ iOS-Accessibility-Sampler/App/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+
         return true
     }
 
